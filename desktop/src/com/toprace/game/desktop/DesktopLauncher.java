1362261a6877b6d3874cc39232dc3611557cc25c
Alban36 <albanchauvin36@gmail.com>
2016-06-16 01:00:08 -0400

    Initial commit du projet. Celui-ci est vide.

---
+++ desktop/src/com/toprace/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.toprace.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.toprace.game.TopRaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TopRaceGame(), config);
+	}
+}
