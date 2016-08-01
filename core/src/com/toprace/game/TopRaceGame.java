8b4413bccc61445a6a25e7241d5eacae577a7499
Alban Chauvin <albanchauvin36@gmail.com>
2016-08-01 13:21:51 -0400

    Ajout des denières modifications avant début d'un premier prototype

--- core/src/com/toprace/game/TopRaceGame.java
+++ core/src/com/toprace/game/TopRaceGame.java
@@ -1,8 +1,13 @@
 package com.toprace.game;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
@@ -16,9 +21,12 @@ import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Matrix3;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.Bullet;
+import com.badlogic.gdx.physics.bullet.collision.Collision;
 import com.badlogic.gdx.physics.bullet.collision.ContactListener;
 import com.badlogic.gdx.physics.bullet.collision.btBoxShape;
 import com.badlogic.gdx.physics.bullet.collision.btBroadphaseInterface;
@@ -56,8 +64,6 @@ public class TopRaceGame extends ApplicationAdapter {
         //public AssetManager assets;
         //public boolean loading;
         public Model model;
-        public ModelInstance ground;
-        public ModelInstance car;
         public boolean collision;
         public btCollisionConfiguration collisionConfig;
         public btDispatcher dispatcher;
@@ -67,31 +73,73 @@ public class TopRaceGame extends ApplicationAdapter {
         btBroadphaseInterface broadphase;
         btDynamicsWorld dynamicsWorld;
         btConstraintSolver constraintSolver;
+        int ObjectCounter;
+        boolean pause = false;
+        public FPSLogger fpslogger;
+        float angle, speed = 90f;
+        public AssetManager assets; 
+        boolean loading;
+        Matrix4 OriginCarTransform;
         
         
-        class MyContactListener extends ContactListener {
+        /*class MyContactListener extends ContactListener {
                 @Override
-                public boolean onContactAdded (int userValue0, int partId0, int index0, int userValue1, int partId1, int index1) {
-                    if (userValue0 != 0)
-                        ((ColorAttribute)instances.get(userValue0).materials.get(0).get(ColorAttribute.Diffuse)).color.set(Color.WHITE);
-                    if (userValue1 != 0)
-                        ((ColorAttribute)instances.get(userValue1).materials.get(0).get(ColorAttribute.Diffuse)).color.set(Color.WHITE);
+                public boolean onContactAdded (int userValue0, int partId0, int index0, boolean match0, int userValue1, int partId1, int index1, boolean match1) {
+                    if (match0)
+                    {
+                        for (GameObject obj : instances)
+                        {
+                            if(obj.body.getUserValue()==userValue0)
+                            {
+                                ((ColorAttribute)obj.materials.get(0).get(ColorAttribute.Diffuse)).color.set(Color.WHITE);
+                            }
+                        }
+                    }
+                    
+                    if (match1)
+                    {
+                        for (GameObject obj : instances)
+                        {
+                            if(obj.body.getUserValue()==userValue1)
+                            {
+                                ((ColorAttribute)obj.materials.get(0).get(ColorAttribute.Diffuse)).color.set(Color.WHITE);
+                            }
+                        }
+                    }
                     return true;
             }
         }
         
-        MyContactListener contactListener;
+        MyContactListener contactListener;*/
         
         static class GameObject extends ModelInstance implements Disposable {
             public final btRigidBody body;
+            public final MyMotionState motionState;
+            
             public GameObject(Model model, String node, btRigidBodyConstructionInfo constructionInfo) {
-                super(model, node);
-                body = new btRigidBody(constructionInfo);
+                super(model);
+                motionState = new MyMotionState();
+                motionState.transform = transform;
+                if(constructionInfo != null){
+                    body = new btRigidBody(constructionInfo);
+                    body.setMotionState(motionState);
+                }
+                else
+                {
+                    body = null;
+                }
+            }
+            
+            @Override
+            public boolean equals(Object object)
+            {
+                return object instanceof GameObject && ((GameObject)object).body == this.body && ((GameObject)object).motionState == this.motionState;
             }
 
             @Override
             public void dispose () {
                 body.dispose();
+                motionState.dispose();
             }
             
             static class Constructor implements Disposable {
@@ -136,7 +184,6 @@ public class TopRaceGame extends ApplicationAdapter {
             }
         }
 
-        
 	@Override
 	public void create () {
             Bullet.init();
@@ -147,110 +194,142 @@ public class TopRaceGame extends ApplicationAdapter {
             
             modelBatch = new ModelBatch();
             
-            cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            cam.position.set(3f, 7f, 10f);
-            cam.lookAt(0, 4f, 0);
+            cam = new PerspectiveCamera(90, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            cam.position.set(0f, 30f, 0f);
+            cam.lookAt(0f, 0f, 0f);
             cam.near = 1f;
             cam.far = 300f;
             cam.update();
             
-            camController = new CameraInputController(cam);
-            Gdx.input.setInputProcessor(camController);
-            
-            
+            //camController = new CameraInputController(cam);
+            //Gdx.input.setInputProcessor(camController);
             
             ModelBuilder mb = new ModelBuilder();
             mb.begin();
             mb.node().id = "ground";
-            mb.part("ground", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.RED)))
-                .box(10f, 1f, 10f);
-            mb.node().id = "sphere";
-            mb.part("sphere", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.GREEN)))
-                .sphere(1f, 1f, 1f, 10, 10);
-            mb.node().id = "box";
-            mb.part("box", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.BLUE)))
-                .box(1f, 1f, 1f);
-            mb.node().id = "cone";
-            mb.part("cone", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.YELLOW)))
-                .cone(1f, 2f, 1f, 10);
-            mb.node().id = "capsule";
-            mb.part("capsule", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.CYAN)))
-                .capsule(0.5f, 2f, 10);
-            mb.node().id = "cylinder";
-            mb.part("cylinder", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.MAGENTA)))
-                .cylinder(1f, 2f, 1f, 10);
+            mb.part("ground", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.GRAY)))
+                .box(50f, 1f, 50f);
             model = mb.end();
 
             constructors = new ArrayMap<String, GameObject.Constructor>(String.class, GameObject.Constructor.class);
-            constructors.put("ground", new GameObject.Constructor(model, "ground", new btBoxShape(new Vector3(5f, 0.5f, 5f)),0f));
-            constructors.put("sphere", new GameObject.Constructor(model, "sphere", new btSphereShape(0.5f),1f));
-            constructors.put("box", new GameObject.Constructor(model, "box", new btBoxShape(new Vector3(0.5f, 0.5f, 0.5f)),1f));
-            constructors.put("cone", new GameObject.Constructor(model, "cone", new btConeShape(0.5f, 2f),1f));
-            constructors.put("capsule", new GameObject.Constructor(model, "capsule", new btCapsuleShape(.5f, 1f),1f));
-            constructors.put("cylinder", new GameObject.Constructor(model, "cylinder", new btCylinderShape(new Vector3(.5f, 1f, .5f)),1f));
-
+            constructors.put("ground", new GameObject.Constructor(model, "ground", new btBoxShape(new Vector3(25f, 0.5f, 25f)),0f));
+            
             collisionConfig = new btDefaultCollisionConfiguration();
             dispatcher = new btCollisionDispatcher(collisionConfig);
             broadphase = new btDbvtBroadphase();
             constraintSolver = new btSequentialImpulseConstraintSolver();
             dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, constraintSolver, collisionConfig);
             dynamicsWorld.setGravity(new Vector3(0, -10f, 0));
-            contactListener = new MyContactListener();
+            //contactListener = new MyContactListener();
+            
+            ObjectCounter = 0;
             
             instances = new Array<GameObject>();
+            instances.ordered=false;
             GameObject object = constructors.get("ground").construct();
+            object.body.setUserValue(ObjectCounter);
             instances.add(object);
-            dynamicsWorld.addRigidBody(object.body, GROUND_FLAG, ALL_FLAG);
+            dynamicsWorld.addRigidBody(object.body);
+            object.body.setContactCallbackFlag(GROUND_FLAG);
+            object.body.setContactCallbackFilter(0);
             
-            /*assets = new AssetManager();
+            fpslogger = new FPSLogger();
+            
+            assets = new AssetManager();
             assets.load("data/car.obj", Model.class);
-            loading = true;*/
+            loading = true;
 	}
         
-        /*private void doneLoading() {
-        Model car = assets.get("data/car.obj", Model.class);
-        ModelInstance carInstance = new ModelInstance(car); 
-        instances.add(carInstance);
-        loading = false;
-    }*/
+        private void doneLoading() {
+            ObjectCounter++;
+            Model car = assets.get("data/car.obj", Model.class);
+            
+            constructors.put("car", new GameObject.Constructor(car, "car", new btBoxShape(new Vector3(2f, 1.5f, 2f)),1f));
+            
+            GameObject obj = constructors.get("car").construct();
+ 
+            obj.transform.trn(0f, 5f, 0f);
+            obj.body.proceedToTransform(obj.transform);
+            obj.body.setUserValue(ObjectCounter);
+            obj.body.setCollisionFlags(obj.body.getCollisionFlags() | btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK);
+            
+            instances.add(obj);
+            dynamicsWorld.addRigidBody(obj.body);
+            obj.body.setContactCallbackFlag(OBJECT_FLAG);
+            obj.body.setContactCallbackFilter(GROUND_FLAG);
+            
+            OriginCarTransform = obj.transform;
+            
+            loading = false;
+            System.out.format("DoneLoading !\n");
+        }
 
 	@Override
 	public void render () {
-            /*if (loading && assets.update())
-            doneLoading();*/
+
+            fpslogger.log();
             
-            final float delta = Math.min(1f/30f, Gdx.graphics.getDeltaTime());
+            if (loading && assets.update()){
+                doneLoading();
+            }
+            
+            if(!loading)
+            {
+            
+                if (Gdx.input.isKeyPressed(Keys.UP)) {
+                    Vector3 Force = new Vector3(0f,0f,-1f);
+                    Matrix4 worldTrans = new Matrix4();
+                    instances.get(1).body.getMotionState().getWorldTransform(worldTrans);
+                    Matrix3 temp = new Matrix3();
+                    temp.set(worldTrans);
+                    Vector3 correctedForce = Force.mul(temp);
+                    instances.get(1).body.applyCentralImpulse(correctedForce);
+                }
+
+                if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+                    Vector3 Force = new Vector3(0f,0f,1f);
+                    Matrix4 worldTrans = new Matrix4();
+                    instances.get(1).body.getMotionState().getWorldTransform(worldTrans);
+                    Matrix3 temp = new Matrix3();
+                    temp.set(worldTrans);
+                    Vector3 correctedForce = Force.mul(temp);
+                    instances.get(1).body.applyCentralImpulse(correctedForce);
+                }
+
+                if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+                    instances.get(1).transform.rotate(Vector3.Y, 1f);
+                    instances.get(1).body.proceedToTransform(instances.get(1).transform);
+                }
+                
+                if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+                    instances.get(1).transform.rotate(Vector3.Y, -1f);
+                    instances.get(1).body.proceedToTransform(instances.get(1).transform);
+                }
+
+                Vector3 CarPosition = new Vector3();
+                instances.get(1).body.getWorldTransform().getTranslation(CarPosition);
+                if(CarPosition.y < -5)
+                {
+                    instances.get(1).body.clearForces();
+                    instances.get(1).transform = OriginCarTransform;
+                    instances.get(1).body.proceedToTransform(instances.get(1).transform);
+                }
+                
+                //instances.get(1).body.getTotalForce()
+            }
+
+            final float delta = Math.min(1f/30f, Gdx.graphics.getDeltaTime());              
 
             dynamicsWorld.stepSimulation(delta, 5, 1f/60f);
-            
-            for (GameObject obj : instances) {
-                obj.body.getWorldTransform(obj.transform);
-            }
 
-            if ((spawnTimer -= delta) < 0) {
-                spawn();
-                spawnTimer = 1.5f;
-            }
-        
-            camController.update();
+            //camController.update();
             Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
             modelBatch.begin(cam);
             modelBatch.render(instances, environment);
             modelBatch.end();
-	}
-        
-        public void spawn() {
-                GameObject obj = constructors.values[1+MathUtils.random(constructors.size-2)].construct();
-                obj.transform.setFromEulerAngles(MathUtils.random(360f), MathUtils.random(360f), MathUtils.random(360f));
-                obj.transform.trn(MathUtils.random(-2.5f, 2.5f), 9f, MathUtils.random(-2.5f, 2.5f));
-                obj.body.setWorldTransform(obj.transform);
-                obj.body.setUserValue(instances.size);
-                obj.body.setCollisionFlags(obj.body.getCollisionFlags() | btCollisionObject.CollisionFlags.CF_CUSTOM_MATERIAL_CALLBACK);
-                instances.add(obj);
-                dynamicsWorld.addRigidBody(obj.body, OBJECT_FLAG, ALL_FLAG);
-            }
+	}    
         
         @Override
         public void dispose() {
@@ -273,8 +352,9 @@ public class TopRaceGame extends ApplicationAdapter {
 
             modelBatch.dispose();
             model.dispose();
-            contactListener.dispose();
-            //assets.dispose();
+            
+            //contactListener.dispose();
+            assets.dispose();
         }
         
         @Override
